704f3a9e75ad22cccbb6835bde68bde06ed55917
ggsivakumar <ggsivakumar@gmail.com>
2020-07-16 17:39:38 +0800

    AutoMapper Issues Resolved

---
+++ vHotelAPI/Models/DataTransferObjects/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace vHotelAPI.Models
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryDto>();
+
+            CreateMap<Product, ProductDto>();
+
+            CreateMap<NewCategoryDto, Category>();
+
+            CreateMap<UpdateCategoryDto, Category>();
+        }
+    }
+}
